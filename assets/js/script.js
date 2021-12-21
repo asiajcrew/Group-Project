6e141f24611d3ae714bc307db9d374cadf3ccc62
asiajcrew <asia_jcrew96@yahoo.com>
2021-12-21 18:07:13 -0500

    added js code to generate random joke on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,39 @@
-var buttonEl = document.querySelector(".btn");
+var apiUrl = "https://api.humorapi.com/jokes/random?api-key=905745e1181e46198869ccbf66b4d0dd";
+var displayEl = document.getElementById("display-joke");
+var buttonEl = document.getElementById("new-joke");
 
-var apiUrl = "https://humorapi.com/docs/#Random-Joke"
-var joke = fetch(apiUrl)
-    .then(function(response) {
-        if (response.ok) {
-            console.log(response);
-            response.json().then(function(data) {
-                console.log(data);
-                display(data);
-            });
-        }
-    });
+function callapi() {
+    fetch(apiUrl)
+.then(function(res) {
+    return res.json();
+})
+.then(function(data) {
+    console.log(data)
+    displayEl.textContent = data.joke;
+    // "joke" in data.joke refers the joke line in the console log 
+})
+.catch(function(error) {
+    console.log(error);
+})
+}
 
-var titleEl = document.createElement("span");
-buttonEl.addEventListener("submit", fetch);
+buttonEl.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log(event)
+    callapi();
+});
+
+callapi();
+// remove this call if we do not want a joke to display when the page loads
 
 
 
 
 
+// Notes from tutoring:
+// var whatever = [];
 
-
-
+// whatever.push("hello")
+// console.log(whatever)
 
 // 2nd api https://placebear.com/g/200/300
